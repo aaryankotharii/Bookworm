920a5ee3958d9ef1ae3167fad8fa531b836dcdc4
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-01 19:46:01 +0530

    @Binding learnt

--- BookWorm/ContentView.swift
+++ BookWorm/ContentView.swift
@@ -8,9 +8,32 @@
 
 import SwiftUI
 
+struct pushButton: View{
+    
+    let title : String
+    @Binding var isOn : Bool
+    
+    var onColors = [Color.pink,Color.yellow]
+    var offColors = [Color(white : 0.6),
+                     Color(white : 0.4)]
+    
+    var body: some View{
+        Button(title){
+            self.isOn.toggle()
+        }.padding()
+            .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .bottomLeading, endPoint: .topTrailing))
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
 struct ContentView: View {
+    @State var rememberMe = false
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            pushButton(title: "RememberMe", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
     }
 }
 
