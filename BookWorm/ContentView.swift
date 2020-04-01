bcbad9af4940f9f940ea6e5264a48177aab3a1ce
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-01 23:38:53 +0530

    AddBook Form complete

--- BookWorm/ContentView.swift
+++ BookWorm/ContentView.swift
@@ -8,6 +8,41 @@
 
 import SwiftUI
 
+
+
+struct ContentView: View{
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books : FetchedResults<Book>
+    @State private var showingAddScreen = false
+    
+    var body : some View{
+        NavigationView{
+            Text("Count: \(books.count)")
+            .navigationBarTitle("BookWorm")
+                .navigationBarItems(trailing: Button(action: {
+                    self.showingAddScreen.toggle()
+                }){
+                    Image(systemName: "plus")
+                }).sheet(isPresented: $showingAddScreen){
+                    AddBookView().environment(\.managedObjectContext, self.moc)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
+
+
+
+
+
 //struct pushButton: View{
 //
 //    let title : String
@@ -36,16 +71,3 @@ import SwiftUI
 //        }
 //    }
 //}
-
-struct ContentView: View{
-    @Environment(\.horizontalSizeClass) var sizeClass
-    var body : some View{
-        Text(sizeClass.debugDescription)
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
